2d0f87730e38ab5912aae16f888ab742efeb8b37
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-06-27 21:51:05 +0300

    feat: welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Parallax } from "@react-spring/parallax"
 import Header from "./compents/Header"
+import Welcome from "./compents/Welcome"
 
 function App() {
   return <>
     <Header />
+    <Parallax pages={5} className="top-0 left-0">
+      <Welcome />
+    </Parallax>
   </>
 }
 
