b789c23802be594171a2ff15dc2090dd3f6202bc
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-06-29 14:10:30 +0300

    style: shape animation & svg sizes

--- src/pages/Welcome/Shapes/index.jsx
+++ src/pages/Welcome/Shapes/index.jsx
@@ -9,27 +9,42 @@ const Shapes = () => {
   return <>
     <ParallaxLayer offset={0} speed={1.25}>
       <div className="screen max-w-full center relative">
-        <img src={shapeBottomRight} alt="Shape 0" className="select-none w-1/3 opacity-80 absolute right-12 bottom-0" />
+        <img src={shapeBottomRight} alt="Shape 0"
+          className="floating select-none w-1/4 max-md:w-1/2 opacity-80 absolute md:right-12 right-0 bottom-0"
+          style={{ animationDelay: "-200ms" }}
+        />
       </div>
     </ParallaxLayer>
     <ParallaxLayer offset={0} speed={1.5}>
       <div className="screen max-w-full center relative">
-        <img src={shapeBottomLeft} alt="Shape 2" className="select-none w-1/3 opacity-80 absolute bottom-0 left-0" />
-      </div>
-    </ParallaxLayer>
-    <ParallaxLayer offset={0} speed={1.15}>
-      <div className="screen max-w-full center relative">
-        <img src={shapeCenter} alt="Shape 1" className="select-none min-w-[480px] w-1/2 opacity-80" />
+        <img src={shapeBottomLeft} alt="Shape 2"
+          className="floating select-none w-1/4 max-md:w-1/2 opacity-80 absolute left-0 bottom-0"
+          style={{ animationDelay: "-400ms" }}
+        />
       </div>
     </ParallaxLayer>
     <ParallaxLayer offset={0} speed={.125}>
       <div className="screen max-w-full center relative">
-        <img src={shapeTopRight} alt="Shape 2" className="select-none w-1/3 opacity-80 absolute top-0 right-8" />
+        <img src={shapeTopRight} alt="Shape 2"
+          className="floating select-none w-1/4 max-md:w-1/2 opacity-80 absolute md:right-8 right-0 top-0"
+          style={{ animationDelay: "-300ms" }}
+        />
       </div>
     </ParallaxLayer>
     <ParallaxLayer offset={0} speed={.15}>
       <div className="screen max-w-full center relative">
-        <img src={shapeTopLeft} alt="Shape 2" className="select-none w-1/3 opacity-80 absolute top-0 left-8" />
+        <img src={shapeTopLeft} alt="Shape 2"
+          className="floating select-none w-1/4 max-md:w-1/2 opacity-80 absolute md:left-8 left-0 top-0"
+          style={{ animationDelay: "-800ms" }}
+        />
+      </div>
+    </ParallaxLayer>
+    <ParallaxLayer offset={0} speed={1.15}>
+      <div className="screen max-w-full center relative">
+        <img src={shapeCenter} alt="Shape 1"
+          className="floating select-none min-w-[75vh] w-1/2 opacity-80"
+          style={{ animationDelay: "-00ms" }}
+        />
       </div>
     </ParallaxLayer>
   </>
