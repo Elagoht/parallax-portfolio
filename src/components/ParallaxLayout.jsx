ad973ffa2119e5403969c9ce45979a89bf6da22b
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-06-29 13:25:41 +0300

    chore: about text now less sticky

--- src/components/ParallaxLayout.jsx
+++ src/components/ParallaxLayout.jsx
@@ -5,15 +5,18 @@ import WhatIDo from "../pages/About/Articles/WhatIDo"
 import WhoAmI from "../pages/About/WhoAmI"
 import TechStack from "../pages/About/Articles/TechStack"
 import Shapes from "../pages/Welcome/Shapes"
+import Background from "../pages/About/Background"
+
 
 const ParallaxLayout = () => {
   return <Parallax pages={10} className="top-0 left-0">
     <Shapes />
     <ParallaxLayer offset={0} speed={.75}> <Welcome /> </ParallaxLayer>
-    <ParallaxLayer offset={1} sticky={{ start: 1, end: 5.5 }}> <About /> </ParallaxLayer>
-    <ParallaxLayer sticky={{ start: 1.25, end: 2 }}> <WhoAmI /> </ParallaxLayer>
-    <ParallaxLayer sticky={{ start: 3, end: 3.75 }}> <WhatIDo /> </ParallaxLayer>
-    <ParallaxLayer sticky={{ start: 4.75, end: 5.5 }}> <TechStack /> </ParallaxLayer>
+    <ParallaxLayer sticky={{ start: 1, end: 6 }}> <Background /> </ParallaxLayer>
+    <ParallaxLayer sticky={{ start: 1, end: 1.25 }} > <About /> </ParallaxLayer>
+    <ParallaxLayer sticky={{ start: 1.75, end: 2.5 }} id="about"> <WhoAmI /> </ParallaxLayer>
+    <ParallaxLayer sticky={{ start: 3.5, end: 4.25 }}> <WhatIDo /> </ParallaxLayer>
+    <ParallaxLayer sticky={{ start: 5.25, end: 6 }}> <TechStack /> </ParallaxLayer>
   </Parallax >
 }
 
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,15 +1,8 @@
-import shapeBackground from "./Shape.svg"
-
 const About = () => {
 
   return <>
     <section className="flex center screen max-w-none p-0">
-      <div
-        id="about" className="container center items-start pt-24 max-w-none h-screen bg-top bg-no-repeat bg-contain max-lg:bg-cover"
-        style={{ backgroundImage: `url(${shapeBackground})` }}
-      >
-        <h1 className="text-neutral-800 mt-8">About</h1>
-      </div>
+      <h1 className="text-neutral-800 mt-8">About</h1>
     </section >
   </>
 }
